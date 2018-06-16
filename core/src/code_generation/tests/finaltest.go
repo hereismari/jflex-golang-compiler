6995303e7fd1825917cec953f000a800284294b3
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2018-06-16 10:51:10 -0300

    Adding finaltest.go

---
+++ core/src/code_generation/tests/finaltest.go
@@ -0,0 +1,34 @@
+package name;
+
+var a int = 10;
+var b int = 2;
+
+func block() bool {
+    if a > b {
+        return true && false;
+        if 1 >= 2 {
+            return false;
+        } else {
+            return false;
+        };
+    } else if (b > 3) {
+    } else if (b > 3) {
+        a = 3;
+    } else {
+    
+    };
+};
+
+func factorial(x int) int {
+  if (x <= 1) {
+    return 1;
+  } else {
+    return factorial#(x-1) * x;
+  };
+};
+
+func main() {
+  a += 2;
+  a = factorial#(a);
+  block#();
+};
