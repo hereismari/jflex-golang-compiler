73ce166208c68ff74e469c0fc9d23fb0ca36060a
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2018-06-16 09:28:21 -0300

    Addind SpecificTestSemantic

--- core/src/code_generation/SpecificTestCodeGenerator.java
+++ core/src/code_generation/SpecificTestCodeGenerator.java
@@ -1,16 +1,8 @@
 package code_generation;
 
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.file.Paths;
-
-
 import java_cup.runtime.ComplexSymbolFactory;
-import lexical.Lexer;
-import semantic.exceptions.SemanticException;
 import semantic.Semantic;
-import syntax.Parser;
+import semantic.SpecificTestSemantic;
 
 
 public class SpecificTestCodeGenerator {
@@ -30,46 +22,14 @@ public class SpecificTestCodeGenerator {
 		   return "";
 		}
 	}
-
-	public static void parse(String sourceCode, ComplexSymbolFactory csf) throws Exception {
-		Semantic.getInstance().clear();
-		Semantic.getInstance().getCodeGenerator().init();
-		
-		boolean exception = false;
-		
-		try {
-			FileInputStream stream = new FileInputStream(sourceCode);
-			Reader reader = new InputStreamReader(stream);
-	
-			Lexer lexer = new Lexer(reader, csf);
-			// start parsing
-			Parser p = new Parser(lexer, csf);
-			System.out.println("Parsing: " + sourceCode);
-			p.parse();
-			System.out.println("Parsing finished!");
-		} catch (SemanticException e) {
-			e.printStackTrace();
-			System.err.println(e.getMessage());
-			
-			if(!sourceCode.contains("FAIL")) {
-				throw new Exception(sourceCode + " was not parsed correctly but it should!");
-			}
-			
-			exception = true;
-		}
-		
-		if(sourceCode.contains("FAIL") && !exception) {
-			throw new Exception(sourceCode + " should fail but it was parsed correctly :/");
-		}
-	}
 	
 	public static void main(String[] args) throws Exception {
 		ComplexSymbolFactory csf = new ComplexSymbolFactory();
 		
-		String filePath = Paths.get("").toAbsolutePath().toString() + "/core/src/code_generation/tests/finaltest.go";
-		String outputFilename = Paths.get("").toAbsolutePath().toString() + "/core/src/code_generation/tests/finaltest.asm";
+		String filePath = SpecificTestSemantic.getAbsolutePath("/core/src/code_generation/tests/finaltest.go");
+		String outputFilename = SpecificTestSemantic.getAbsolutePath("/core/src/code_generation/tests/finaltest.asm");
         
-		parse(filePath, csf);
+		SpecificTestSemantic.parse(filePath, csf);
 		Semantic.getInstance().getCodeGenerator().generateFinalAssemblyCode(outputFilename);
 		
 		System.out.println("----------------------------------");
--- core/src/code_generation/TestCodeGenerator.java
+++ core/src/code_generation/TestCodeGenerator.java
@@ -1,25 +1,17 @@
 package code_generation;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.file.Paths;
-
-
 import java_cup.runtime.ComplexSymbolFactory;
-import lexical.Lexer;
-import semantic.exceptions.SemanticException;
 import semantic.Semantic;
-import syntax.Parser;
+import semantic.SpecificTestSemantic;
 
 
 public class TestCodeGenerator {
 	public static void main(String[] args) throws Exception {
 		ComplexSymbolFactory csf = new ComplexSymbolFactory();
 		
-		String rootPath = Paths.get("").toAbsolutePath().toString() + "/core/src/code_generation/tests/";
-		String outputPath = Paths.get("").toAbsolutePath().toString() + "/core/src/code_generation/tests/";
+		String rootPath = SpecificTestSemantic.getAbsolutePath("/core/src/code_generation/tests/");
+		String outputPath = SpecificTestSemantic.getAbsolutePath("/core/src/code_generation/tests/");
         
 		File folder = new File(rootPath);
 		File[] listOfFiles = folder.listFiles();
@@ -27,7 +19,7 @@ public class TestCodeGenerator {
 		for (int i = 0; i < listOfFiles.length; i++) {
 			String filename = listOfFiles[i].getName();
 			if (listOfFiles[i].isFile() && SpecificTestCodeGenerator.getExtension(filename).equals("go")) {
-				SpecificTestCodeGenerator.parse(rootPath + filename, csf);
+				SpecificTestSemantic.parse(rootPath + filename, csf);
 				String outputFilename = outputPath + SpecificTestCodeGenerator.removeExtension(filename) + ".asm";
 				Semantic.getInstance().getCodeGenerator().generateFinalAssemblyCode(outputFilename);
 			}
